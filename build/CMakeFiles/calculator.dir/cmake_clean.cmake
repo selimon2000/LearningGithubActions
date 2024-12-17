cdbf5464ebf79ee458879e46e976fa042f16b427
selimon2000 <selimon.s@outlook.com>
2024-12-17 16:16:09 +1100

    committing to a merge only branch

---
+++ build/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/calculator.cpp.o"
+  "CMakeFiles/calculator.dir/calculator.cpp.o.d"
+  "CMakeFiles/calculator.dir/main.cpp.o"
+  "CMakeFiles/calculator.dir/main.cpp.o.d"
+  "calculator"
+  "calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
